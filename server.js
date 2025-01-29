df2f334b82f0ab19dfb8a650e3a9497c8a128c70
lironhefcode <lironheftman@gmail.com>
2025-01-29 14:20:23 +0200

    user backend

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express' 
 import cookieParser from 'cookie-parser'
 import {bugService} from './services/bug.service.js'
+import { userService } from './services/user.service.js'
+import { authService } from './services/auth.service.js'
 const app = express() 
 
 app.use(express.static('public'))
@@ -82,4 +84,58 @@ app.delete('/api/bug/:id', (req, res) => {
             res.status(500).send('Cannot remove bug')
         })
 
+})
+
+//      USER!
+app.get('/api/user',(req,res) =>{
+    userService.query()
+                        .then(users => res.send(users))
+                        .catch(err => {
+                            res.status(400).send('Cannot load users')
+                        })
+})
+
+
+app.get('/api/user/:userId', (req,res)=>{
+    const { userId } = req.params
+     userService.getById(userId)
+                                .then(user => res.send(user))
+                                .catch(err =>{
+                                    res.status(400).send('user not found')
+                                })
+})
+
+///      AUTH API!!!
+
+app.post('/api/auth/login', (req,res) =>{
+    const creden = req.body
+    authService.checkLogin(creden)
+                                .then(user =>{
+                                    const loginToken = authService.getLoginToken(user) 
+                                    res.cookie('loginToken', loginToken)
+                                    res.send(user)
+                                })
+                                .catch(() => res.status(404).send('Invalid Credentials'))
+})
+
+
+app.post('/api/auth/signup', (req, res) => {
+    const credentials = req.body
+    
+    userService.add(credentials)
+        .then(user => {
+            if (user) {
+                const loginToken = authService.getLoginToken(user)
+                res.cookie('loginToken', loginToken)
+                res.send(user)
+            } else {
+                res.status(400).send('Cannot signup')
+            }
+        })
+        .catch(err => res.status(400).send('Username taken.'))
+})
+
+app.post('/api/auth/logout', (req, res) => {
+    res.clearCookie('loginToken')
+    res.send('logged-out!')
 })
